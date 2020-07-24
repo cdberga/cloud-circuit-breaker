f042be86839e278125f873cdde6243b08f2c8ba3
Carlos <carlos.bergamasco@gmail.com>
2020-07-24 15:59:38 -0300

    Adjust BookService path and circuit breaker lib

--- reading/src/main/java/hello/ReadingApplication.java
+++ reading/src/main/java/hello/ReadingApplication.java
@@ -6,11 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @SpringBootApplication
 public class ReadingApplication {
+
+	@Autowired
+	private BookService bookService;
 
 	@RequestMapping("/to-read")
 	public Mono<String> toRead() {
